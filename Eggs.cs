ef82732a7124949dc007b36a849c603877ad1405
Udoka Ozonevo <udokaozonevo3018@gmail.com>
2025-11-16 16:18:28 -0800

    Correct variable names and override method in Eggs
    Fix variable naming and method override issues in Eggs class.

--- Eggs.cs
+++ Eggs.cs
@@ -3,3 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks
+
+namespace Grocery_List
+{
+  internal class Eggs
+  {
+    // Data Members
+    private double unit price;
+    private int quantity;
+
+    public Eggs()                      // Default Constructor
+    {
+      unit_price = 0.0;
+      quantity = 0;
+    }
+    public Eggs(double price, int qn)  // Overloading Constructor
+    {
+      unit_price = price;
+      quantity = qn;
+    }
+
+    // Properties
+    public double UnitPrice
+    {
+      get {return unit_price;}
+      set {unit_price = value;}
+    }
+    public int Quantity
+    {
+      get {return quantity;}
+      set {quantity = value;}
+    }
+    public double total_Price()
+    {
+      return unit_price * quantity;
+    }
+    public overrride string ToString()
+    {
+      return "\nEggs Quantity: " + quantity + 
+              "Unit Price: $" + unit_price;
+    }
+    ~Eggs() {}                           // Destructor
+  }
+}
